a62d5f0a57150d7ece33f9dfe6e3af44c99dec85
MAYURNISHANE <mayur.reflectiontp@gmail.com>
2022-07-18 13:24:03 +0530

    calculation js

---
+++ 18072022/calculation.js
@@ -0,0 +1,11 @@
+var num1 = '20';
+var num2 = '60';
+var num3 = '40';
+var sum = (num1 + num2) + num3;
+var sub = num2 - num1;
+var divide = num1 / num3;
+
+
+document.write('Addition: ', sum, '<br/>');
+document.write('substraction: ', sub, '<br/>');
+document.write('division: ', divide, '<br/>');
