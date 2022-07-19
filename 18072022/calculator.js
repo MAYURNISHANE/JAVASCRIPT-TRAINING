35465b1a36c36810dce9d606ffe063e32b79ce85
MAYURNISHANE <mayur.reflectiontp@gmail.com>
2022-07-19 10:17:43 +0530

    calculator js

---
+++ 18072022/calculator.js
@@ -0,0 +1,36 @@
+function addition() {
+    var num1 = document.getElementById('num1').value;
+    var num2 = document.getElementById('num2').value;
+    var sum = parseInt(num1) + parseInt(num2);
+    document.getElementById('result').innerHTML = sum;
+}
+function substraction() {
+    var num1 = document.getElementById('num1').value;
+    var num2 = document.getElementById('num2').value;
+    var sum = num1 - num2;
+    document.getElementById('result').innerHTML = sum;
+}
+function multiply() {
+    var num1 = document.getElementById('num1').value;
+    var num2 = document.getElementById('num2').value;
+    var sum = num1 * num2;
+    document.getElementById('result').innerHTML = sum;
+}
+function divide() {
+    var num1 = document.getElementById('num1').value;
+    var num2 = document.getElementById('num2').value;
+    var sum = num1 / num2;
+    document.getElementById('result').innerHTML = sum;
+}
+function modulus() {
+    var num1 = document.getElementById('num1').value;
+    var num2 = document.getElementById('num2').value;
+    var sum = num1 % num2;
+    document.getElementById('result').innerHTML = sum;
+}
+function clearscreen() {
+    var num1 = document.getElementById('num1').value;
+    var num2 = document.getElementById('num2').value;
+    var sum = '';
+    document.getElementById('result').innerHTML = sum;
+}
