3f37dc476d4a95f084b7a07376c1ed305f750789
MAYURNISHANE <mayur.reflectiontp@gmail.com>
2022-07-24 21:43:20 +0530

    Switch exam.

---
+++ 21072022/switch.js
@@ -0,0 +1,44 @@
+function showSubNav(id) {
+    switch (id) {
+        case 'accessories':
+            if (document.getElementById(id).style.display == 'none') {
+                document.getElementById(id).style.display = 'block';
+            }
+            else {
+                document.getElementById(id).style.display = 'none';
+            }
+            break;
+        case 'cars':
+            if (document.getElementById(id).style.display == 'none') {
+                document.getElementById(id).style.display = 'block';
+            }
+            else {
+                document.getElementById(id).style.display = 'none';
+            }
+            break;
+        case 'mobiles':
+            if (document.getElementById(id).style.display == 'none') {
+                document.getElementById(id).style.display = 'block';
+            }
+            else {
+                document.getElementById(id).style.display = 'none';
+            }
+            break;
+        case 'laptops':
+            if (document.getElementById(id).style.display == 'none') {
+                document.getElementById(id).style.display = 'block';
+            }
+            else {
+                document.getElementById(id).style.display = 'none';
+            }
+            break;
+        case 'jwellery':
+            if (document.getElementById(id).style.display == 'none') {
+                document.getElementById(id).style.display = 'block';
+            }
+            else {
+                document.getElementById(id).style.display = 'none';
+            }
+            break;
+    }
+}
