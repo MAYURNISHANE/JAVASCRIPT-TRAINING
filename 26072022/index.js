7aed17bc0078e4ce9da8929614eaff61557c0876
MAYURNISHANE <mayur.reflectiontp@gmail.com>
2022-07-30 14:35:52 +0530

    image slide move for usin button and numbers

--- 26072022/index.js
+++ 26072022/index.js
@@ -1,5 +1,15 @@
 var images = ['image/image2.png', 'image/image3.png', 'image/banner3.png', 'image/banner2.png', 'image/banner1.png']
 var imagetag = '';
+
+var dots = '';
+for (var i = 0; i < images.length; i++) {
+    dots += '<span onclick="showimage(' + i + ')" class="block">' + i + '</span>'
+}
+document.getElementById('dots').innerHTML = dots;
+function showimage(id) {
+    document.getElementById('mayur').innerHTML = '<img id="image' + (id) + '"  data-pics="' + (id) + '" src="' + images[(id)] + '" />'
+}
+
 for (var i = 0; i < images.length; i++) {
     imagetag += '<div class="images" id="image' + i + '"><img class="img" src ="' + images[i] + '"/></div>'
 }
@@ -15,13 +25,13 @@ function next() {
     }
     document.getElementById('image' + parseInt(id + 1)).style.display = 'block';
 }
-function prev(){
+function prev() {
     var id = '';
-for (var i = 0; i < images.length; i++) {
-    if (document.getElementById('image' + i).style.display == 'block') {
-        id = i;
+    for (var i = 0; i < images.length; i++) {
+        if (document.getElementById('image' + i).style.display == 'block') {
+            id = i;
+        }
+        document.getElementById('image' + i).style.display = 'none';
     }
-    document.getElementById('image' + i).style.display = 'none';
-}
-document.getElementById('image' + parseInt(id - 1)).style.display = 'block';
+    document.getElementById('image' + parseInt(id - 1)).style.display = 'block';
 }
