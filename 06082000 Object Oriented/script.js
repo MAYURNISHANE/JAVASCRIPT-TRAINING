b24508e71b46b5eb8980d9a8a130f79721313da7
MAYURNISHANE <mayur.reflectiontp@gmail.com>
2022-08-06 15:39:48 +0530

    Constructor   Object Oriented

---
+++ 06082000 Object Oriented/script.js
@@ -0,0 +1,16 @@
+class slider {
+    constructor() {
+        this.name ='Mayur'
+        alert(this.name);
+    }
+    autoslide = () => {
+        alert('autoslide');
+    }
+    next = () => {
+        alert('next');
+    }
+    prev = () => {
+        alert('prev')
+    }
+}
+let user = new slider();
